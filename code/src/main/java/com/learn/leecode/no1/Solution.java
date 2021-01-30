85b0d8889c2acc580bd2973bbdb0fca15dfedc6f
deific <deific@126.com>
2021-01-30 22:48:11 +0800

    feat: 重新整理leecode代码

---
+++ code/src/main/java/com/learn/leecode/no1/Solution.java
@@ -0,0 +1,71 @@
+
+package com.learn.leecode.no1;
+
+public class Solution {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 你可以按任意顺序返回答案。
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * 示例 2：
+     *
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     * 示例 3：
+     *
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     *  
+     *
+     * 提示：
+     *
+     * 2 <= nums.length <= 103
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int[] index = new int[nums.length];
+        int[] targetIndex = new int[2];
+        int secode = 0;
+        int secodeIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+            secode = target - nums[i];
+            secodeIndex = secode % nums.length;
+            if (secodeIndex > 0) {
+                targetIndex[0] = i;
+                targetIndex[1] = secodeIndex;
+                return targetIndex;
+            } else {
+                index[secodeIndex] = i;
+            }
+        }
+
+        return targetIndex;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[4];
+        nums[0] = 2;
+        nums[1] = 7;
+        nums[2] = 4;
+        nums[3] = 7;
+
+        int[] result = twoSum(nums, 9);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
